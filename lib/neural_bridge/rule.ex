d898c726f30fa0c929008bf7526d1ace08b72ee0
lorenzosinisi <lasslo.net@gmail.com>
2020-06-03 19:18:07 +0200

    Add a test that asserts it works

---
+++ lib/neural_bridge/rule.ex
@@ -0,0 +1,55 @@
+defmodule NeuralBridge.Rule do
+  import Retex.Facts
+  defstruct [:id, :given, :then]
+
+  def new(id: id, given: given, then: then) when is_binary(given) and is_binary(then) do
+    with {:ok, given} <- to_production(given),
+         {:ok, then} <- to_production(then) do
+      %__MODULE__{id: id, given: given, then: then}
+    end
+  end
+
+  def new(id: id, given: given, then: then) when is_binary(given) and is_function(then) do
+    with {:ok, given} <- to_production(given) do
+      %__MODULE__{id: id, given: given, then: then}
+    end
+  end
+
+  defp to_production(conditions) when is_binary(conditions) do
+    with {:ok, ast} <- parse(conditions) do
+      {:ok, interpret(ast)}
+    end
+  end
+
+  defp parse(str) do
+    Sanskrit.parse(str)
+  end
+
+  defp interpret(ast) when is_list(ast) do
+    for node <- ast, do: do_interpret(node)
+  end
+
+  defp do_interpret({:filter, type, kind, value}) do
+    filter(type, kind, value)
+  end
+
+  defp do_interpret({:unexistant_attribute, type, attr}) do
+    unexistant_attribute(type, attr)
+  end
+
+  defp do_interpret({:negation, variable, type}) do
+    is_not(variable, type)
+  end
+
+  defp do_interpret({:wme, type, attr, value}) do
+    Retex.Wme.new(type, attr, value)
+  end
+
+  defp do_interpret({:isa, var, type}) do
+    isa(var, type)
+  end
+
+  defp do_interpret({:has_attribute, type, attr, kind, value}) do
+    has_attribute(type, attr, kind, value)
+  end
+end
---
+++ test/engine_test.exs
@@ -0,0 +1,109 @@
+defmodule NeuralBridge.EngineTest do
+  use ExUnit.Case
+  alias Retex.{RuleEngine, Wme}
+  import ExUnit.CaptureLog
+  alias NeuralBridge.{Engine}
+  alias NeuralBridge.Rule
+  alias RuleEngine, as: Engine
+
+  test "can be started" do
+    assert Engine.new("test")
+  end
+
+  test "rules can be added, and their conclusion can be a function call" do
+    engine = Engine.new("test")
+
+    rules = [
+      Rule.new(
+        id: 1,
+        given: """
+        Person's name is equal "bob"
+        """,
+        then: """
+        Person's age is 23
+        """
+      ),
+      Rule.new(
+        id: 2,
+        given: """
+        Person's name is equal $name
+        Person's age is equal 23
+        """,
+        then: fn production ->
+          require Logger
+          bindings = Map.get(production, :bindings)
+          Logger.info(inspect(bindings))
+        end
+      )
+    ]
+
+    engine = Engine.add_rules(engine, rules)
+    assert Enum.empty?(engine.rule_engine.agenda)
+
+    engine = Engine.add_facts(engine, Wme.new("Person", "name", "bob"))
+
+    rule = List.first(engine.rule_engine.agenda)
+
+    assert Enum.empty?(engine.rules_fired)
+
+    assert %Retex.Node.PNode{
+             action: [
+               %Retex.Wme{
+                 attribute: "age",
+                 id: _,
+                 identifier: "Person",
+                 timestamp: nil,
+                 value: 23
+               }
+             ]
+           } = rule
+
+    assert engine = Engine.apply_rule(engine, rule)
+
+    refute Enum.empty?(engine.rules_fired)
+    assert fun = Enum.find(engine.rule_engine.agenda, fn pnode -> is_function(pnode.action) end)
+
+    assert capture_log(fn ->
+             Engine.apply_rule(engine, fun)
+           end) =~ inspect(%{"$name" => "bob"})
+  end
+
+  test "rules can be added" do
+    engine = Engine.new("test")
+
+    rules = [
+      Rule.new(
+        id: 1,
+        given: """
+        Person's name is equal "bob"
+        """,
+        then: """
+        Person's age is 23
+        """
+      )
+    ]
+
+    engine = Engine.add_rules(engine, rules)
+    assert Enum.empty?(engine.rule_engine.agenda)
+
+    engine = Engine.add_facts(engine, Wme.new("Person", "name", "bob"))
+
+    rule = List.first(engine.rule_engine.agenda)
+
+    assert Enum.empty?(engine.rules_fired)
+
+    assert %Retex.Node.PNode{
+             action: [
+               %Retex.Wme{
+                 attribute: "age",
+                 id: _,
+                 identifier: "Person",
+                 timestamp: nil,
+                 value: 23
+               }
+             ]
+           } = rule
+
+    assert engine = Engine.apply_rule(engine, rule)
+  end
+end

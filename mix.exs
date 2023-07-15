bebd6079958017a15ff69082f29f0a5d5f56fd47
lorenzosinisi <lasslo.net@gmail.com>
2023-07-15 23:40:30 +0200

    Add licence

--- mix.exs
+++ mix.exs
@@ -24,6 +24,15 @@ defmodule NeuralBridge.MixProject do
     """
   end
 
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README.md", "LICENSE"],
+      maintainers: ["Lorenzo Sinisi"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/lorenzosinisi/retex"}
+    ]
+  end
+
   # Run "mix help compile.app" to learn about applications.
   def application do
     [

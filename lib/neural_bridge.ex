db29bfe88a723bcf8c0e3bdadf94ff02ba163e3c
lorenzosinisi <lasslo.net@gmail.com>
2023-07-15 23:38:40 +0200

    Cleanup meaningless test

--- lib/neural_bridge.ex
+++ lib/neural_bridge.ex
@@ -1,18 +1,2 @@
 defmodule NeuralBridge do
-  @moduledoc """
-  Documentation for `NeuralBridge`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> NeuralBridge.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/neural_bridge_test.exs
+++ test/neural_bridge_test.exs
@@ -1,8 +1,4 @@
 defmodule NeuralBridgeTest do
   use ExUnit.Case
   doctest NeuralBridge
-
-  test "greets the world" do
-    assert NeuralBridge.hello() == :world
-  end
 end
